b743db5dafb031fca88beab04c7e0200f626afea
adamantcm <adamantiosminas@gmail.com>
2020-03-28 22:33:17 +0200

    Added BatchProvider, wrestling with vanishing gradients

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
 from nn import NeuralNetwork
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import normalize
+import numpy as np
+import matplotlib.pyplot as plt
 
-x = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# x, y = load_breast_cancer(return_X_y=True)
+# x = np.asarray(x)
+#
+# corrs = np.corrcoef(x, y, rowvar=False)
+# indexed = []
+#
+# for c in corrs[-1][:-1]:
+#     indexed.append((c, len(indexed)))
+#
+# indexed.sort(key=lambda x: x[0])
+#
+# cols = []
+# for i in range(8):
+#     cols.append(indexed[i][1])
+#
+# # x = x[:, cols]
+#
+# print(x.shape)
+#
+# x = normalize(x)
+# print(y)
 
-y = [1, 2, 3]
 
-net = NeuralNetwork([3, 2, 1])
+def load_planar_dataset():
+    m = 400  # number of examples
+    N = int(m / 2)  # number of points per class
+    D = 2  # dimensionality
+    X = np.zeros((m, D))  # data matrix where each row is a single example
+    Y = np.zeros((m, 1), dtype='uint8')  # labels vector (0 for red, 1 for blue)
+    a = 4  # maximum ray of the flower
+
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+
+    return X, Y
+
+np.random.seed(1)
+x, y = load_planar_dataset()
+# plt.figure()
+# plt.scatter(x[:, 0], x[:, 1], c=y[:, 0], s=40, cmap=plt.cm.Spectral)
+# plt.show()
+# print(x.shape, y.shape)
+
+net = NeuralNetwork([4, 1], epochs=10000)
 net.fit(x, y)
--- nn.py
+++ nn.py
@@ -1,9 +1,10 @@
 import numpy as np
-
+import os
+import shutil
+from tabulate import tabulate
 
 def relu(x):
     return np.maximum(x, 0)
-
 
 def relu_derivative(x):
     _x = np.copy(x)
@@ -13,21 +14,87 @@ def relu_derivative(x):
 
     return _x
 
+def sigmoid(x):
+    x = np.clip(x, a_min=1e-7, a_max=None)
+    r = 1 / (1 + np.exp(-x))
+    return r
+
+def sigmoid_derivative(x):
+    r = sigmoid(x) * (1 - sigmoid(x))
+    return r
+
+def tanh(x):
+    return np.tanh(x)
+
+def tanh_derivative(x):
+    return 1 - tanh(x) ** 2
 
 def entropy_loss(y_pred, y_true):
     y_pred = np.clip(y_pred, 1e-7, 1.0-1e-7)
     return -(np.sum((y_true * np.log(y_pred) + (1-y_true)*np.log(1-y_pred))) / y_pred.shape[1])
 
-
 def entropy_loss_derivative(y_pred, y_true):
+    y_pred = np.clip(y_pred, 1e-7, 1.0-1e-7)
     return -(y_true/y_pred) + ((1-y_true)/(1-y_pred))
+
+def dump_values(x, a, z, w, dw, db, epoch):
+    dir = "./epoch_{}_values".format(epoch)
+
+    if os.path.exists(dir) and os.path.isdir(dir):
+        shutil.rmtree(dir)
+
+    os.makedirs(dir)
+    data = {"a": a, "z": z, "w": w, "dw": dw, "db": db}
+
+    for d in data:
+        vals = data[d]
+
+        for index, arr in enumerate(vals):
+            with open(os.path.join(dir, "{}{}".format(d, index+1)), "w") as f:
+                table = tabulate(arr)
+                f.write(table + "\n")
+
+    with open(os.path.join(dir,"x"), "w") as f:
+        table = tabulate(x)
+        f.write(table + "\n")
+
+class BatchProvider:
+    def __init__(self, data, batch_size):
+        self.data = data
+        self.no_examples = len(data)
+        self.batch_size = batch_size
+        self.index = 0
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index >= self.no_examples:
+            raise StopIteration
+
+        start = self.index
+        end = self.index + self.batch_size
+        if end > self.no_examples:
+            end = self.no_examples
+
+        self.index += self.batch_size
+
+        return self.data[start:end]
+
 
 
 class NeuralNetwork(object):
-    def __init__(self, layer_units, lr=0.0001, batch_size=-1):
+    def __init__(self, layer_units, lr=1.2, epochs=1, batch_size=-1):
         self.layer_units = layer_units
+        layers = len(layer_units)
+
+        self._activation_functions = [tanh if i != layers - 1 else sigmoid for i in range(layers)]
+        self._activation_function_derivatives = [tanh_derivative if i != layers - 1 else sigmoid_derivative for i in range(layers)]
+
         self.lr = lr
         self.batch_size = batch_size
+        self.epochs = epochs
 
         self._weights = []
         self._biases = []
@@ -36,11 +103,13 @@ class NeuralNetwork(object):
         self._z_values_cache = []
 
     def _initialize_weights(self, no_features):
+        np.random.seed(2)
         # Initialize weights
         prev_units = no_features
         for units in self.layer_units:
-            w = np.random.random((units, prev_units))
-            b = np.random.random((units, 1))
+            w = np.random.randn(units, prev_units)*0.01
+            print(w)
+            b = np.zeros((units, 1))
 
             self._weights.append(w)
             self._biases.append(b)
@@ -50,16 +119,41 @@ class NeuralNetwork(object):
     def fit(self, train_x, train_y):
         # Get number of features of each training example
         no_features = len(train_x[0])
+        no_examples = len(train_x)
 
-        x = np.asarray(train_x).T
-        y = np.asarray(train_y).reshape(1, -1)
+        x = np.asarray(train_x)
+        y = np.asarray(train_y)
+
+        if self.batch_size == -1:
+            self.batch_size = no_examples
+
+        x_batches = BatchProvider(x, self.batch_size)
+        y_batches = BatchProvider(y, self.batch_size)
 
         self._initialize_weights(no_features)
 
-        self._forward_propagation(x)
-        self._backpropagation(y)
-        
+        for i in range(self.epochs):
+            print("Epoch " + str(i))
+            for x_batch, y_batch in zip(x_batches, y_batches):
+                x_batch = x_batch.T
+                y_batch = y_batch.reshape(1, -1)
+
+                self._forward_propagation(x_batch)
+                dw, db = self._backpropagation(y_batch)
+                # dump_values(x_batch, self._a_values_cache, self._z_values_cache, self._weights, dw, db, i)
+                self._update_weights(dw, db)
+
+            self._forward_propagation(x.T)
+            preds = self._a_values_cache[-1]
+            print(preds.shape)
+            loss = self._loss(preds, y.reshape(1, -1))
+            print(loss)
+
+
     def _forward_propagation(self, x):
+        self._z_values_cache.clear()
+        self._a_values_cache.clear()
+
         layers = len(self.layer_units)
 
         prev_activations = x
@@ -67,12 +161,9 @@ class NeuralNetwork(object):
             w = self._weights[i]
             b = self._biases[i]
 
-            print(w.shape, prev_activations.shape, b.shape)
-
             z = np.matmul(w, prev_activations) + b
-            a = self._activation_function(z)
 
-            print(a.shape)
+            a = self._activation_functions[i](z)
 
             prev_activations = a
 
@@ -98,8 +189,7 @@ class NeuralNetwork(object):
                 # dA[l-1] = dL/dA[l] * dA[l]/dZ[l] * dZ[l]/dA[l-1] = dL/dZ[l] * W[l] = W[l].T * dZ[l]
                 da = np.matmul(self._weights[i+1].T, dz) # dz will be defined from previous iteration
 
-            dz = da * self._activation_function_derivative(self._z_values_cache[i])
-
+            dz = da * self._activation_function_derivatives[i](self._z_values_cache[i])
             # Calculate dw and db
             # dw = dL/dA[l] * dA[l]/dZ[l] * dZ[l]/dW[l] = 1/m * dZ[l] * A[l-1].T
             dw = (1/batch_size) * np.matmul(dz, self._a_values_cache[i-1].T)
@@ -109,7 +199,14 @@ class NeuralNetwork(object):
             dw_array.append(dw)
             db_array.append(db)
 
-        return reversed(dw_array), reversed(db_array)
+        return list(reversed(dw_array)), list(reversed(db_array))
+
+    def _update_weights(self, dw, db):
+        layers = len(self.layer_units)
+
+        for i in range(layers):
+            self._weights[i] = self._weights[i] - self.lr * dw[i]
+            self._biases[i] = self._biases[i] - self.lr * db[i]
 
     def _activation_function(self, z):
         return relu(z)
