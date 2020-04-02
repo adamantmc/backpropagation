Removed clipping from sigmoid, which resulted in wrong derivatives, added gradient checking, added sum for multiple outputs

main.py
@@ -3,7 +3,70 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import normalize
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+import os
 
+# Data from: https://www.kaggle.com/c/dogs-vs-cats/data
+def load_dataset(image_folder="images/", split_ratio=0.7):
+    labels_dict = {"cat": 0, "dog": 1}
+    labels_dict = {"cat": [0, 1], "dog": [1, 0]}
+
+    vectors_pickle_file = "image_vectors.pickle"
+    labels_pickle_file = "image_labels.pickle"
+
+    if os.path.exists(vectors_pickle_file) and os.path.exists(labels_pickle_file):
+        print("Loading image vectors from {}".format(vectors_pickle_file))
+        images = pickle.load(open(vectors_pickle_file, "rb"))
+        print("Loading image labels from {}".format(labels_pickle_file))
+        labels = pickle.load(open(labels_pickle_file, "rb"))
+    else:
+        from PIL import Image
+        from keras.applications.resnet50 import ResNet50, preprocess_input
+        from keras import Model
+
+        resnet = ResNet50(weights='imagenet')
+        feature_model = Model(inputs=resnet.input, outputs=resnet.get_layer("flatten_1").output)
+
+        files = [os.path.join(image_folder, filename) for filename in os.listdir(image_folder) if
+                 os.path.isfile(os.path.join(image_folder, filename))][:100]
+        np.random.shuffle(files)
+
+        labels = [labels_dict[os.path.basename(f).split(".")[0]] for f in files]
+        images = None
+
+        step = 2000
+        for i in range(0, len(files), step):
+            file_batch = files[i:i + step]
+
+            batch_images = []
+
+            for filename in file_batch:
+                img = Image.open(filename).resize((224, 224), Image.ANTIALIAS)
+                img = np.asarray(img, dtype="float64")
+                batch_images.append(preprocess_input(img.copy()))
+
+            image_features = feature_model.predict(np.asarray(batch_images))
+
+            if images is None:
+                images = image_features
+            else:
+                images = np.concatenate((images, image_features), axis=0)
+
+        pickle.dump(images, open(vectors_pickle_file, "wb"))
+        pickle.dump(labels, open(labels_pickle_file, "wb"))
+
+    train_last_index = int(split_ratio * len(images))
+    no_classes = len(labels_dict["cat"]) if type(labels_dict["cat"]) == list else 1
+
+    train_x = np.asarray(images[:train_last_index])
+    train_y = np.asarray(labels[:train_last_index]).reshape(-1, no_classes)
+
+    test_x = np.asarray(images[train_last_index:])
+    test_y = np.asarray(labels[train_last_index:]).reshape(-1, no_classes)
+
+    return train_x, train_y, test_x, test_y
+
+x, y, test_x, test_y = load_dataset("images/")
 # x, y = load_breast_cancer(return_X_y=True)
 # x = np.asarray(x)
 #
@@ -28,6 +91,7 @@ import matplotlib.pyplot as plt
 
 
 def load_planar_dataset():
+    np.random.seed(1)
     m = 400  # number of examples
     N = int(m / 2)  # number of points per class
     D = 2  # dimensionality
@@ -44,12 +108,8 @@ def load_planar_dataset():
 
     return X, Y
 
-np.random.seed(1)
-x, y = load_planar_dataset()
-# plt.figure()
-# plt.scatter(x[:, 0], x[:, 1], c=y[:, 0], s=40, cmap=plt.cm.Spectral)
-# plt.show()
-# print(x.shape, y.shape)
-
-net = NeuralNetwork([4, 1], epochs=10000)
+# np.random.seed(1)
+# x, y = load_planar_dataset()
+net = NeuralNetwork([2], epochs=10000)
+print(x.shape, y.shape)
 net.fit(x, y)

nn.py
@@ -15,7 +15,6 @@ def relu_derivative(x):
     return _x
 
 def sigmoid(x):
-    x = np.clip(x, a_min=1e-7, a_max=None)
     r = 1 / (1 + np.exp(-x))
     return r
 
@@ -30,12 +29,20 @@ def tanh_derivative(x):
     return 1 - tanh(x) ** 2
 
 def entropy_loss(y_pred, y_true):
-    y_pred = np.clip(y_pred, 1e-7, 1.0-1e-7)
-    return -(np.sum((y_true * np.log(y_pred) + (1-y_true)*np.log(1-y_pred))) / y_pred.shape[1])
+    y_pred = np.clip(y_pred, 1e-7, 1.0 - 1e-7)
+    return -(np.sum(
+        np.sum(
+            (
+                y_true * np.log(y_pred) +
+                (1-y_true) * np.log(1-y_pred)
+            ),
+            axis=0,
+            keepdims=True
+        )
+    ) / y_pred.shape[1])
 
 def entropy_loss_derivative(y_pred, y_true):
-    y_pred = np.clip(y_pred, 1e-7, 1.0-1e-7)
-    return -(y_true/y_pred) + ((1-y_true)/(1-y_pred))
+    return -(np.divide(y_true, y_pred) - np.divide(1-y_true, 1-y_pred))
 
 def dump_values(x, a, z, w, dw, db, epoch):
     dir = "./epoch_{}_values".format(epoch)
@@ -57,6 +64,7 @@ def dump_values(x, a, z, w, dw, db, epoch):
     with open(os.path.join(dir,"x"), "w") as f:
         table = tabulate(x)
         f.write(table + "\n")
+
 
 class BatchProvider:
     def __init__(self, data, batch_size):
@@ -83,9 +91,8 @@ class BatchProvider:
         return self.data[start:end]
 
 
-
 class NeuralNetwork(object):
-    def __init__(self, layer_units, lr=1.2, epochs=1, batch_size=-1):
+    def __init__(self, layer_units, lr=0.0001, epochs=1, batch_size=-1):
         self.layer_units = layer_units
         layers = len(layer_units)
 
@@ -107,8 +114,7 @@ class NeuralNetwork(object):
         # Initialize weights
         prev_units = no_features
         for units in self.layer_units:
-            w = np.random.randn(units, prev_units)*0.01
-            print(w)
+            w = np.zeros((units, prev_units))
             b = np.zeros((units, 1))
 
             self._weights.append(w)
@@ -136,43 +142,50 @@ class NeuralNetwork(object):
             print("Epoch " + str(i))
             for x_batch, y_batch in zip(x_batches, y_batches):
                 x_batch = x_batch.T
-                y_batch = y_batch.reshape(1, -1)
+                y_batch = y_batch.T
+                z_cache, a_cache = self._forward_propagation(x_batch, self._weights, self._biases)
 
-                self._forward_propagation(x_batch)
-                dw, db = self._backpropagation(y_batch)
+                self._z_values_cache = z_cache
+                self._a_values_cache = a_cache
+                dw, db = self._backpropagation(x_batch, y_batch)
+
+                # self._gradient_check(x_batch, y_batch, self._weights, self._biases, dw, db)
                 # dump_values(x_batch, self._a_values_cache, self._z_values_cache, self._weights, dw, db, i)
+
                 self._update_weights(dw, db)
 
-            self._forward_propagation(x.T)
+            self._forward_propagation(x.T, self._weights, self._biases)
             preds = self._a_values_cache[-1]
-            print(preds.shape)
-            loss = self._loss(preds, y.reshape(1, -1))
+            loss = self._loss(preds, y.T)
             print(loss)
 
+        print(self._a_values_cache[-1])
 
-    def _forward_propagation(self, x):
-        self._z_values_cache.clear()
-        self._a_values_cache.clear()
-
-        layers = len(self.layer_units)
+    def _forward_propagation(self, x, weights, biases):
+        z_cache = []
+        a_cache = []
 
         prev_activations = x
-        for i in range(layers):
-            w = self._weights[i]
-            b = self._biases[i]
+        for i, units in enumerate(self.layer_units):
+            w = weights[i]
+            assert w.shape[0] == units
+            b = biases[i]
+            assert b.shape[0] == units
 
             z = np.matmul(w, prev_activations) + b
+            assert z.shape == (units, self.batch_size)
 
             a = self._activation_functions[i](z)
+            assert a.shape == z.shape
 
             prev_activations = a
 
-            self._z_values_cache.append(z)
-            self._a_values_cache.append(a)
+            z_cache.append(z)
+            a_cache.append(a)
 
-        return self._z_values_cache, self._a_values_cache
+        return z_cache, a_cache
 
-    def _backpropagation(self, y):
+    def _backpropagation(self, x, y):
         dw_array = []
         db_array = []
 
@@ -189,17 +202,51 @@ class NeuralNetwork(object):
                 # dA[l-1] = dL/dA[l] * dA[l]/dZ[l] * dZ[l]/dA[l-1] = dL/dZ[l] * W[l] = W[l].T * dZ[l]
                 da = np.matmul(self._weights[i+1].T, dz) # dz will be defined from previous iteration
 
+            assert da.shape[0] == self.layer_units[i]
+            assert da.shape[1] == self.batch_size
+
             dz = da * self._activation_function_derivatives[i](self._z_values_cache[i])
+            assert dz.shape[0] == self.layer_units[i]
+            assert dz.shape[1] == self.batch_size
+
             # Calculate dw and db
             # dw = dL/dA[l] * dA[l]/dZ[l] * dZ[l]/dW[l] = 1/m * dZ[l] * A[l-1].T
-            dw = (1/batch_size) * np.matmul(dz, self._a_values_cache[i-1].T)
+            prev_activations = self._a_values_cache[i-1] if i != 0 else x
+            dw = (1/batch_size) * np.matmul(dz, prev_activations.T)
+            assert dw.shape == self._weights[i].shape
+
             # db = dL/dA[l] * dA[l]/dZ[l] * dZ[l]/db[l] = 1/m * sum(dZ[l])
             db = (1/batch_size) * np.sum(dz, axis=1, keepdims=True)
+            assert db.shape == self._biases[i].shape
 
             dw_array.append(dw)
             db_array.append(db)
 
         return list(reversed(dw_array)), list(reversed(db_array))
+
+    def _gradient_check(self, x, y, weights, biases, dw, db):
+        layers = len(self.layer_units)
+        epsilon = 1e-6
+
+        for l in range(layers):
+            wl = weights[l]
+            for i in range(wl.shape[0]):
+                for j in range(wl.shape[1]):
+                    w = weights[l][i][j]
+
+                    weights[l][i][j] = w + epsilon
+                    right_z, right_a = self._forward_propagation(x, weights, biases)
+                    weights[l][i][j] = w - epsilon
+                    left_z, left_a = self._forward_propagation(x, weights, biases)
+
+                    weights[l][i][j] = w
+
+                    right_loss = self._loss(right_a[-1], y)
+                    left_loss = self._loss(left_a[-1], y)
+
+                    approx_dw = (right_loss - left_loss) / (2 * epsilon)
+                    assert approx_dw - dw[l][i][j] < 1e-7
+
 
     def _update_weights(self, dw, db):
         layers = len(self.layer_units)